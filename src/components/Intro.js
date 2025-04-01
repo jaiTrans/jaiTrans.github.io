cef3e9a582af9ee5f1f84f578005e077233656d5
jaiTrans <jaitrans45@gmail.com>
2025-04-01 12:10:40 +0530

    Update Intro.js

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -5,35 +5,46 @@ import { Link } from 'react-router-dom';
 const Intro = () => {
     return (
         <>
-                <div className="m-auto max-w-6xl p-2 md:p-12 h-5/6" id='about' >
-
-                    <div className="flex flex-col-reverse lg:flex-row py-8 justify-between lg:text-left" data-aos="fade-up">
-                        <div className="lg:w-1/2 flex flex-col lg:mx-4 justify-center">
-                            <img alt="card img" className="rounded-t float-right" src={img} />
-                        </div>
-                        <div className="flex-col my-4 text-center lg:text-left lg:my-0 lg:justify-end w-full lg:w-1/2 px-8" data-aos="zoom-in" data-aos-delay="500">
-                            
-                            <h3 className="text-3xl  text-blue-900 font-bold">Our operational features are:</h3>
-                            <div  className='my-3 text-xl text-gray-600 font-semibold'>          
-                                    <ul>
-                                     <li>1) 24X7 working.</li>
+            <div className="m-auto max-w-6xl p-2 md:p-12 h-5/6" id="about">
+                <div className="flex flex-col-reverse lg:flex-row py-8 justify-between" data-aos="fade-up">
+                    
+                    {/* Left Image Section */}
+                    <div className="lg:w-1/2 flex flex-col lg:mx-4 justify-center">
+                        <img alt="card img" className="rounded-t" src={img} />
+                    </div>
+                    
+                    {/* Right Content Section */}
+                    <div className="flex-col my-4 text-left lg:text-left lg:my-0 lg:justify-end w-full lg:w-1/2 px-8" data-aos="zoom-in" data-aos-delay="500">
+                        
+                        <h3 className="text-3xl text-blue-900 font-bold">Our operational features are:</h3>
+                        
+                        <div className="my-3 text-xl text-gray-600 font-semibold">          
+                            <ul>
+                                <li>1) 24X7 working.</li>
                                 <li>2) Largest supply chain network.</li>
-                                <li>3) Well trained and experienced manpower.</li>
-                                <li>4) Company owned fleet solution</li></ul>
-                            </div>
-                            
-                            <div>
-                                <p className='my-3 text-xl text-gray-600 font-semibold'>The core of our value system is also the motto of our organization viz, ‘Grow together’. Our pioneering efforts have a distinctive mark in market which is apparent from the level of trust our customers put in us.</p>
-                            </div>
-                            <Link to="/contact" className="text-white bg-blue-700 hover:bg-blue-900 inline-flex items-center justify-center w-full px-6 py-2 my-4 text-lg shadow-xl rounded-2xl sm:w-auto sm:mb-0 group">
-                                Contact us
-                                <svg className="w-4 h-4 ml-1 group-hover: translate-x-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-                            </Link>
+                                <li>3) Well-trained and experienced manpower.</li>
+                                <li>4) Company-owned fleet solution.</li>
+                            </ul>
                         </div>
+                        
+                        <div>
+                            <p className="my-3 text-xl text-gray-600 font-semibold">
+                                The core of our value system is also the motto of our organization viz, ‘Grow together’. Our pioneering efforts have a distinctive mark in the market, which is apparent from the level of trust our customers put in us.
+                            </p>
+                        </div>
+                        
+                        <Link to="/contact" className="text-white bg-blue-700 hover:bg-blue-900 inline-flex items-center justify-center w-full px-6 py-2 my-4 text-lg shadow-xl rounded-2xl sm:w-auto sm:mb-0 group">
+                            Contact us
+                            <svg className="w-4 h-4 ml-1 group-hover:translate-x-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path>
+                            </svg>
+                        </Link>
+
                     </div>
                 </div>
+            </div>
         </>
-    )
-}
+    );
+};
 
 export default Intro;
