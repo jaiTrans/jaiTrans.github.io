8a6db5dcfc85b7b46cce04d0a9aa3a0df78d6741
jaiTrans <jaitrans45@gmail.com>
2025-07-13 12:54:25 +0530

    Fix error

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import NavLinks from '../Navbar/NavLinks';
 import { HashLink } from 'react-router-hash-link';
 import Logo from '../../images/JaiTransLogo.jpeg';
 
